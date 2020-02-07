581c09a8e49e2b65bdd2b452ad3fb553a48d1e9a
Haizhan Liu <haizhan.liu@openjawtech.com>
2020-02-07 19:49:18 +0800

    add files

---
+++ src/main/java/app/xlui/example/im/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package app.xlui.example.im.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/broadcast").setViewName("/broadcast");
+        registry.addViewController("/group").setViewName("/group");
+        registry.addViewController("/chat").setViewName("/chat");
+        registry.addViewController("/query").setViewName("/query");
+    }
+}
